f75ae74ac63e99caf97d6d510b134eaa79d70c82
OrIlani055 <orilani055@gmail.com>
2020-02-29 20:10:03 +0200

    update

--- controller/youtubeController.js
+++ controller/youtubeController.js
@@ -188,7 +188,7 @@ module.exports = YoutubePlaylist;
 
 
 // async function ssearchPlaylist(req,res){
-//     let user =  await Googlemodel.findUserByEmail('orilani055@gmail.com');
+//     let user =  await Googlemodel.findUserByEmail('arieell25@gmail.com');
 //     console.log(user[0].google);
 //     oAuth2Client.setCredentials({access_token: user[0].google.access_token});
 //     console.log(oAuth2Client);
